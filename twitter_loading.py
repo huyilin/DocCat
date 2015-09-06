91e5fc5445b5bfe113f5407d276c764ec7909cfc
huyilin <huyilin.ce@gmail.com>
2015-09-06 12:29:10 +0200

    add the tokens and keys

--- twitter_loading.py
+++ twitter_loading.py
@@ -9,10 +9,10 @@ except ImportError:
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
 # Variables that contains the user credentials to access Twitter API
-ACCESS_TOKEN = 'YOUR ACCESS TOKEN"'
-ACCESS_SECRET = 'YOUR ACCESS TOKEN SECRET'
-CONSUMER_KEY = 'YOUR API KEY'
-CONSUMER_SECRET = 'ENTER YOUR API SECRET'
+ACCESS_TOKEN = '3563975957-2VQfMjFpzBWFtkkHxTeMvSAPzE6vlVheyv04Qrv'
+ACCESS_SECRET = 'vnuncQ46DeZ5fDkmAhx9pIvYuvRWJk7vangvy4IZOcmAy'
+CONSUMER_KEY = 'c1F6xszzi0wUSiU9RiMiQY1Se'
+CONSUMER_SECRET = 'VVEo1z2N1upbu1LCQXSLVO2f09gVe9VJCSHirK8ZQyZAkE4Ote'
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
@@ -26,16 +26,14 @@ iterator = twitter_stream.statuses.sample()
 # Here we set it to stop after getting 1000 tweets.
 # You don't have to set it to stop, but can continue running
 # the Twitter API to collect data for days or even longer.
-tweet_count = 1000
+tweet_count = 500
 for tweet in iterator:
     tweet_count -= 1
     # Twitter Python Tool wraps the data returned by Twitter
     # as a TwitterDictResponse object.
     # We convert it back to the JSON format to print/score
-    print json.dumps(tweet)
-
+    if 'text' in tweet:
+        print tweet['text']
     # The command below will do pretty printing for JSON data, try it out
-    # print json.dumps(tweet, indent=4)
-
     if tweet_count <= 0:
         break
