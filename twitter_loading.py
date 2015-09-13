251bfe4950154826541028d60d53d5a8a925e88f
huyilin <huyilin.ce@gmail.com>
2015-09-13 17:40:43 +0200

    changed something

--- twitter_loading.py
+++ twitter_loading.py
@@ -7,39 +7,35 @@ try:
 except ImportError:
     import simplejson as json
 import sys
-reload (sys)
+
+reload(sys)
+
 sys.setdefaultencoding('utf8')
 
 # Import the necessary methods from "twitter" library
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
-import csv
+import ConfigParser
 
-# Variables that contains the user credentials to access Twitter API
-ACCESS_TOKEN = '3461539752-zDImdWiXLQTVHFiKIKG556lMDValtA8jyhDbr7a'
-ACCESS_SECRET = 'JMLJRb5f3gEbfqKIxYh2Rmbl4UZWQIjUPxdNjQnpuHIcg'
-CONSUMER_KEY = '6GdsrUCKYF0abXvGSG2b4neoX'
-CONSUMER_SECRET = 'j9wHCm0a2NA7N4iEwd5W1QK344RB1VGucVfHOi6eb2bMBUjzIP'
+config = ConfigParser.RawConfigParser()
+configFilePath = 'token.conf'
+config.read(configFilePath)
+
+ACCESS_TOKEN = config.get('token', 'ACCESS_TOKEN')
+ACCESS_SECRET = config.get('token', 'ACCESS_SECRET')
+CONSUMER_KEY = config.get('token', 'CONSUMER_KEY')
+CONSUMER_SECRET = config.get('token', 'CONSUMER_SECRET')
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
-# Initiate the connection to Twitter Streaming API
 twitter_stream = TwitterStream(auth=oauth)
 
-# Get a sample of the public data following through Twitter
-# iterator = twitter_stream.statuses.sample()
+#iterator = twitter_stream.statuses.sample()
 iterator = twitter_stream.statuses.filter(locations= "103.516987, 1.243245, 104.993955 , 1.477511", language = "en")
-# Print each tweet in the stream to the screen
-# Here we set it to stop after getting 1000 tweets.
-# You don't have to set it to stop, but can continue running
-# the Twitter API to collect data for days or even longer.
-# this is the case
+
 
 file_size = 2000
 tweet_bag = []
 for tweet in iterator:
-    # Twitter Python Tool wraps the data returned by Twitter
-    # as a TwitterDictResponse object.
-    # We convert it back to the JSON format to print/score
     if 'text' in tweet:
         content = tweet['text'] + '\n'
         tweet_bag.append(content)
