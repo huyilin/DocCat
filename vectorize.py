a308b8568fbf506955c01260d9d4e97141403162
huyilin <huyilin.ce@gmail.com>
2015-11-08 16:03:36 +0100

    update

--- vectorize.py
+++ vectorize.py
@@ -6,12 +6,16 @@ from  sklearn.feature_extraction .text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn import metrics
 
 # load tranning data, a list of documents(strings) and labels
-f = file('trainning_data.p', 'r')
+
+target_names = ['bulling trace', 'nonbullying trace']
+f = file('traning_data.p', 'r')
 data_train = cPickle.load(f)
 corpus_train = data_train[0]
-labels_train = data_train[1]
+labels_train = np.array(data_train[1])
 for i in range(len(corpus_train)):
     corpus_train[i] = ' '.join(corpus_train[i])
     # print corpus[i] + '  => ' + str(labels[i])
@@ -34,34 +38,44 @@ test1_counts = vectorizer.transform(test1_docs)
 test1_tf_idf = tfidf_transformer.transform(test1_counts)
 test1_predicted = classfier.predict(test1_tf_idf)
 
-for doc, label in zip(test1_docs, test1_predicted):
-     print doc + '  => ' + str(label)
+# for doc, label in zip(test1_docs, test1_predicted):
+#      print doc + '  => ' + str(label)
 
 
+
+# loading test data
+f = file('test_data.p', 'r')
+data_test = cPickle.load(f)
+corpus_test = data_test[0]
+labels_test = np.array(data_test[1])
+for i in range(len(corpus_test)):
+    corpus_test[i] = ' '.join(corpus_test[i])
 
 # make the vectorizer => transformer => classifier easier to work with,
 # scikit-learn provides a Pipeline class that behaves like a compound classifier:
+
+# try first with naive bayes classifier
 
 text_classifier = Pipeline([('vect', CountVectorizer()),
                           ('tfidf', TfidfTransformer()),
                           ('clf', MultinomialNB()),])
 
 text_classifier = text_classifier.fit(corpus_train, labels_train)
-
-# see how does it perform for trainning data
-predicted_train = text_classifier.predict(corpus_train)
-print 'accuracy for trainning data : ' + str(np.mean(predicted_train == labels_train))
-
-# using classifer to test the test data and get precision rate
-f = file('test_data.p', 'r')
-data_test = cPickle.load(f)
-corpus_test = data_test[0]
-labels_test = data_test[1]
-for i in range(len(corpus_test)):
-    corpus_test[i] = ' '.join(corpus_test[i])
-
-predicted_train = text_classifier.predict(corpus_train)
 predicted_test = text_classifier.predict(corpus_test)
-print 'accuracy for test data : ' + str(np.mean(predicted_test == labels_test))
+performance = metrics.classification_report(labels_test, predicted_test, [-1 , 1])
+print "performance of using Naive Bayes"
+print performance
+
 
 # Try using the Support Vector Machine
+svm_clf = Pipeline([('vect', CountVectorizer()),
+                      ('tfidf', TfidfTransformer()),
+                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                            alpha=1e-3, n_iter=5, random_state=42)),
+])
+
+svm_clf = svm_clf.fit(corpus_train, labels_train)
+predicted_test = svm_clf.predict(corpus_test)
+performance = metrics.classification_report(labels_test, predicted_test, [-1 , 1])
+print "performance of using Support Vector Machine"
+print performance
