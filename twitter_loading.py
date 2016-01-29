bfaef906b2ae0f4c55cb9a01ea9f3108f2e59822
huyilin <huyilin.ce@gmail.com>
2016-01-29 13:10:22 +0800

    update

--- twitter_loading.py
+++ twitter_loading.py
@@ -30,22 +30,22 @@ CONSUMER_SECRET = config.get('token', 'CONSUMER_SECRET')
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 i = 0
+bully_trace_words = ['bully', 'bullying', 'bullied']
 while True:
     try:
         twitter_stream = TwitterStream(auth=oauth)
-        # iterator = twitter_stream.statuses.filter(locations= "103.588489, 1.233231, 103.994018 , 1.487514", language = "en")
-        iterator = twitter_stream.statuses.filter(language = "en")
+        iterator = twitter_stream.statuses.filter(locations= "103.588489, 1.233231, 103.994018, 1.487514", language = "en")
         for tweet in iterator:
             if 'text' in tweet:
-                content = tweet['created_at'] + ' || ' +tweet['text'] + ' || '' + \n'
-                print content
-            date = str(datetime.utcnow().now().date())
-            file = open('download/' + date + '.csv', 'a')
-            file.write(content)
-            i = i + 1
-            print i
-            file.close()
-            tweet_bag = []
+                if any(word in tweet['text'] for word in bully_trace_words):
+                    content = tweet['created_at'] + ' || ' +tweet['text'] + ' || ' + str(tweet['place']) + '\n'
+                    date = str(datetime.utcnow().now().date())
+                    file = open('download/' + date + '.csv', 'a')
+                    file.write(content)
+                    i = i + 1
+                    print i
+                    print content
+                    file.close()
     except:
         pass
 
